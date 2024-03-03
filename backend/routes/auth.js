43734c4b6cac87bd9adae95e485e3ceb0d641dc6
Hamid Husain <hamid17amu@gmail.com>
2024-03-03 17:24:45 +0530

    createuser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'hello!ndia';
 
 
 router.post('/createuser',[
@@ -11,29 +15,38 @@ router.post('/createuser',[
 ], async (req,res)=>{
     const result = validationResult(req);
     if (result.isEmpty()) {
-        try{
 
-       
-        let user = await User.findOne({email:req.body.email});
-        if(user){
-            return res.status(400).json({error: 'Email already exists!'})
+        try{       
+            let user = await User.findOne({email:req.body.email});
+            if(user){
+                return res.status(400).json({error: 'Email already exists!'})
+            }
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: secPass,
+            })
+            
+            const data={
+                user:{
+                    id:user.id
+                }
+            }
+            // .then((user)=>{res.json(user)})
+            // .catch((error)=>{console.log(error); res.json({error: 'Email already exists', message: error.message})});
+            const authToken = jwt.sign(data, JWT_SECRET);
+            // console.log(jwtdata);
+
+            return res.json({authToken: authToken});
+            // res.send(`Hello, ${req.body.name}!`);
         }
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        })
-
-        // .then((user)=>{res.json(user)})
-        // .catch((error)=>{console.log(error); res.json({error: 'Email already exists', message: error.message})});
         
-        return res.json(user);
-        // res.send(`Hello, ${req.body.name}!`);
-    }
-    catch(error){
-        console.log(error.message);
-        res.status(500).send("some error occured");
-    }
+        catch(error){
+            console.log(error.message);
+            res.status(500).send("some error occured");
+        }
     }
 
     res.send({ errors: result.array() });
